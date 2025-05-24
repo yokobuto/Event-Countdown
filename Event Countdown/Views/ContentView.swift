12c0a3317a8426c0f45de122f17b86dfa2e52ec9
yokobuto <info@yokobuto.de>
2025-05-24 14:35:07 +0200

    Add EventRow and DateFormatter

---
+++ Event Countdown/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Event Countdown
+//
+//  Created by Jörg Klausewitz on 21.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+       
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Event Countdown/Views/EventRow.swift
@@ -0,0 +1,18 @@
+//
+//  EventRow.swift
+//  Event Countdown
+//
+//  Created by Jörg Klausewitz on 24.05.25.
+//
+
+import SwiftUI
+
+struct EventRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventRow()
+}
