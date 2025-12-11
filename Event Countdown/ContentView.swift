01ad8cbb71f644fa3b962b79b858f7c948c09595
yokobuto <info@yokobuto.de>
2025-12-11 17:38:53 +0100

    Initial Commit

---
+++ Event Countdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Event Countdown
+//
+//  Created by Jörg Klausewitz on 11.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Jörg Klausewitz on 11.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
