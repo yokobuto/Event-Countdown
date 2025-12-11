636815144032c8659803d8ab1db973df53cc63cf
yokobuto <info@yokobuto.de>
2025-12-11 18:10:36 +0100

    Event created

---
+++ Event Countdown/Model/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  Event Countdown
+//
+//  Created by Jörg Klausewitz on 11.12.25.
+//
+
+import SwiftUI
+
+struct Event: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Event()
+}
